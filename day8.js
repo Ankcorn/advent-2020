414577d99ab8735563543e36010c48204a4358ca
Ankcorn <thomasankcorn@gmail.com>
2020-12-09 13:29:11 +0000

    Update day8.js

--- day8.js
+++ day8.js
@@ -1,51 +1,49 @@
 const getDataForDay = require('./utils/fetch-challenge-data');
 
 let part1 = (instructions, a, n) => {
-	let next = n || 0
-	let acc = a || 0
-	if(next === instructions.length) {
+	let next = n || 0;
+	let acc = a || 0;
+	if (next === instructions.length) {
 		return { acc, status: 'ended' };
 	}
 
-	if(instructions[next].used) {
+	if (instructions[next].used) {
 		return { acc, status: 'crashed' };
 	}
 
 	instructions[next].used = true
 
-	if(instructions[next].instruction.includes('nop')) {
+	if (instructions[next].instruction.includes('nop')) {
 		return part1(instructions, acc, next + 1)
 	}
 
 	let [i, quant] = instructions[next].instruction.split(' ');
-	let [opp, ...nn] = quant.split('');
-	let num = Number(nn.join(''));
 
-	if(i === 'acc') {
-		return part1(instructions, eval(acc + opp + num), next + 1);
+	if (i === 'acc') {
+		return part1(instructions, eval(acc + quant), next + 1);
 	}
-	if(i === 'jmp') {
-		return part1(instructions, acc, eval(next + opp + num));
+	if (i === 'jmp') {
+		return part1(instructions, acc, eval(next + quant));
 	}
 }
 
 let part2 = (instructions) => {
-	for(let [index, line] of instructions.entries()) {
+	for (let [index, line] of instructions.entries()) {
 		let newIns = JSON.parse(JSON.stringify(instructions))
-		if(line.instruction.includes('jmp')) {
+		if (line.instruction.includes('jmp')) {
 			newIns[index].instruction = line.instruction.replace('jmp', 'nop')
-		} else if(line.instruction.includes('nop')) {
+		} else if (line.instruction.includes('nop')) {
 			newIns[index].instruction = line.instruction.replace('nop', 'jmp')
 		}
 		let res = part1(newIns)
-		if(res.status === 'ended') {
+		if (res.status === 'ended') {
 			return res
 		}
 	}
 }
 
 getDataForDay(8).then(data => {
-	let input = data.trim().split('\n').map(el => ({ instruction: el}));
+	let input = data.trim().split('\n').map(el => ({ instruction: el }));
 	let input2 = JSON.parse(JSON.stringify(input))
 	console.log('part1', part1(input))
 	console.log('part2', part2(input2))
